24cd331ee06d779420672203ed80a8a153a930f5
Yusg0725 <407509797@qq.com>
2018-08-02 11:23:21 +0800

    zbar

--- src/pages/my-zbar/my-zbar.ts
+++ src/pages/my-zbar/my-zbar.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { ZBar,ZBarOptions } from '@ionic-native/zbar';
 
 @IonicPage()
 @Component({
@@ -9,9 +9,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyZbarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private zbar:ZBar,public navCtrl: NavController, public navParams: NavParams) {
+    this.doScanner();
   }
 
+  doScanner(){
+    let options:ZBarOptions={
+      text_title:"扫码",
+      text_instructions:"请把相机对准二维码",
+      camera:"back",
+      flash:"auto",
+      drawSight:true
+    };
+    this.zbar.scan(options)
+    .then(result=>{
+      alert(JSON.stringify(result));
+    })
+    .catch(error=>{
+      console.log(error);
+    });
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad MyZbarPage');
   }
